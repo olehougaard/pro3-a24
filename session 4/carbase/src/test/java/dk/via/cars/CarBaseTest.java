6549787f6500c8fd23e543a0b13f31bcd9d9f988
Ole Hougaard <olehougaard@gmail.com>
2024-09-25 14:27:36 +0200

    Only inline client code

--- session 4/carbase/src/test/java/dk/via/cars/CarBaseTest.java
+++ session 4/carbase/src/test/java/dk/via/cars/CarBaseTest.java
@@ -1,23 +1,18 @@
 package dk.via.cars;
 
 import dk.via.CarbaseApplication;
-import dk.via.cars.business.persistence.Persistence;
-import dk.via.cars.client.dry.Client;
+import dk.via.cars.client.Client;
 import dk.via.cars.model.Car;
 import dk.via.cars.model.Money;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
